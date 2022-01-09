c6db5679ff2f4b3c06a95c67231988461bdfa16e
Kiley Poole <kpoole007@gmail.com>
2022-01-09 10:00:15 -0800

    simple proxy

--- main.go
+++ main.go
@@ -5,34 +5,48 @@ import (
 )
 
 func main() {
-	netAddr := syscall.SockaddrInet4{Port: 8085, Addr: [4]byte{127, 0, 0, 1}}
+	serverAddr := syscall.SockaddrInet4{Port: 8085, Addr: [4]byte{127, 0, 0, 1}}
+	forwardAddr := syscall.SockaddrInet4{Port: 9000}
 
-	socket, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
-	check(err, socket)
+	inboundSocket, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
+	check(err, inboundSocket)
 
-	err = syscall.Bind(socket, &netAddr)
-	check(err, socket)
+	forwardSocket, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
+	check(err, forwardSocket)
 
-	err = syscall.Listen(socket, syscall.SOMAXCONN)
-	check(err, socket)
+	defer syscall.Close(forwardSocket)
+	defer syscall.Close(inboundSocket)
 
-	newSock, incAddr, err := syscall.Accept(socket)
-	check(err, socket)
+	err = syscall.Bind(inboundSocket, &serverAddr)
+	check(err, inboundSocket)
 
-	res := make([]byte, 512)
+	err = syscall.Listen(inboundSocket, syscall.SOMAXCONN)
+	check(err, inboundSocket)
+
+	newSock, _, err := syscall.Accept(inboundSocket)
+	check(err, inboundSocket)
+
+	res := make([]byte, 2048)
+
 	_, _, err = syscall.Recvfrom(newSock, res, 0)
-	check(err, socket)
+	check(err, newSock)
 
-	err = syscall.Sendto(newSock, res, 0, incAddr)
-	check(err, socket)
+	err = syscall.Connect(forwardSocket, &forwardAddr)
+	check(err, forwardSocket)
 
-	err = syscall.Close(socket)
-	check(err, socket)
+	err = syscall.Sendto(forwardSocket, res, 0, &forwardAddr)
+	check(err, forwardSocket)
+
+	_, _, err = syscall.Recvfrom(forwardSocket, res, 0)
+	check(err, newSock)
+
+	err = syscall.Sendto(newSock, res, 0, &forwardAddr)
+	check(err, forwardSocket)
 }
 
 func check(err error, socket int) {
 	if err != nil {
-		syscall.Shutdown(socket, 2)
+		syscall.Close(socket)
 		panic(err)
 	}
 }
